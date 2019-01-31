69529c9369b12a2436051290dc4288c00e43c8c3
mitchellghl <46975869+mitchellghl@users.noreply.github.com>
2019-01-30 23:58:22 -0500

    moved project to git

---
+++ exercises/exercise02/Assets/Scripts/LaunchScript.cs
@@ -0,0 +1,165 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaunchScript : MonoBehaviour {
+	//This variable will store how much force we will launch the shape forward with
+	//NOTE: Because I am 'declaring' the variable here (outside of any function 
+	//		definition), we will be able to access it from any where in this file.
+	float launchForce = 0;
+
+	//These variables are used to store the position and rotation of the shape right when
+	//the game starts. We will reset the transform to these values if the gameObject
+	//collides with a big invisible trigger at the bottom of the scene.
+	//See the OnTriggerEnter function below.
+	Vector3 startPosition;
+	Quaternion startRotation;
+
+	//We will set this to true in the OnTriggerEnter function if the gameObject
+	//goes through the ring.
+	bool playerWon = false;
+    bool ring1 = false;
+    bool ring2 = false;
+    bool ring3 = false;
+
+    public Material SuccessMaterial;
+    GameObject Cube1a;
+    GameObject Cube1b;
+    GameObject Cube1c;
+    GameObject Cube1d;
+
+    GameObject Cube2a;
+    GameObject Cube2b;
+    GameObject Cube2c;
+    GameObject Cube2d;
+
+    GameObject Cube3a;
+    GameObject Cube3b;
+    GameObject Cube3c;
+    GameObject Cube3d;
+
+    // Use this for initialization
+    void Start () {
+		startPosition = transform.position;
+		startRotation = transform.rotation;
+
+        Cube1a = GameObject.Find("Cube1a");
+        Cube1b = GameObject.Find("Cube1b");
+        Cube1c = GameObject.Find("Cube1c");
+        Cube1d = GameObject.Find("Cube1d");
+
+        Cube2a = GameObject.Find("Cube2a");
+        Cube2b = GameObject.Find("Cube2b");
+        Cube2c = GameObject.Find("Cube2c");
+        Cube2d = GameObject.Find("Cube2d");
+
+        Cube3a = GameObject.Find("Cube3a");
+        Cube3b = GameObject.Find("Cube3b");
+        Cube3c = GameObject.Find("Cube3c");
+        Cube3d = GameObject.Find("Cube3d");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		//If the player releases the space bar function, launch the shape!
+		//Description below:
+		if (Input.GetKeyUp(KeyCode.Space)) {
+			//1. Get a reference to the Rigidbody on this GameObject
+			//	 Store it in a variable called rb. rb contains all of the 
+			//   variables and functions that are part of the Rigidbody component
+			//   that we added in the Unity inspector (right side of the screen)
+			Rigidbody rb = gameObject.GetComponent<Rigidbody>();
+			//2. Enable gravity (NOTE: We previously unchecked this in the inspector).
+			//	 By setting rb's useGravity variable to true, we are "rechecking" the
+			//	 box via code.
+			rb.useGravity = true;
+			//3. Shoot the cube in the forward direction scaled by te force value
+			//	 Don't worry too much about the math for now.
+			rb.AddForce(transform.forward * launchForce);
+		}
+
+		//If the player is pressing space, increase the launchForce variable by 3.
+		//Otherwise, decrease it by 3.
+		if (Input.GetKey(KeyCode.Space)) {
+			launchForce = launchForce + 3f;
+		} else {
+			//Only decrease the launch force if it is greater than 0
+			//This prevents the variable from going negative.
+			if (launchForce > 0) {
+				launchForce = launchForce - 3f;
+			}
+		}
+
+        if (ring1 && ring2 && ring3){
+            playerWon = true;
+        }
+	}
+
+	//This function is called by Unity when gameObjects with the following configuration
+	//collide with oneanother:
+	//
+	//Recipe for making Unity call the OnTriggerEnter function:
+	//	1. At least one of the objects needs to have a Rigidbody component
+	//	2. At least one of the objects needs to have a collider marked as a trigger
+	//	3. Any Component with an "OnTriggerEnter" function will have their function called
+	void OnTriggerEnter(Collider other)
+	{
+		//Check to see if the trigger we collided with is the "InvisibleTriggerCube"
+		//that is in the middle of the Ring gameObject.
+		if (other.gameObject.name == "InvisibleTriggerCube") {
+			ring1 = true;//In OnGUI we will print you win when this is set to true
+
+            Cube1a.GetComponent<Renderer>().material = SuccessMaterial;
+            Cube1b.GetComponent<Renderer>().material = SuccessMaterial;
+            Cube1c.GetComponent<Renderer>().material = SuccessMaterial;
+            Cube1d.GetComponent<Renderer>().material = SuccessMaterial;
+        }
+
+        if (other.gameObject.name == "InvisibleTriggerCube2")
+        {
+            ring2 = true;//In OnGUI we will print you win when this is set to true
+
+            Cube2a.GetComponent<Renderer>().material = SuccessMaterial;
+            Cube2b.GetComponent<Renderer>().material = SuccessMaterial;
+            Cube2c.GetComponent<Renderer>().material = SuccessMaterial;
+            Cube2d.GetComponent<Renderer>().material = SuccessMaterial;
+        }
+
+        if (other.gameObject.name == "InvisibleTriggerCube3")
+        {
+            ring3 = true;//In OnGUI we will print you win when this is set to true
+
+            Cube3a.GetComponent<Renderer>().material = SuccessMaterial;
+            Cube3b.GetComponent<Renderer>().material = SuccessMaterial;
+            Cube3c.GetComponent<Renderer>().material = SuccessMaterial;
+            Cube3d.GetComponent<Renderer>().material = SuccessMaterial;
+        }
+
+        if (other.gameObject.name == "InvisibleRespawnCube") {
+			//If we collide with the big invisible cube at the bottom of the scene
+			//reposition the shape, turn off gravity again, and stop its
+			//momentum (otherwise it will keep moving at the velocity it was flying at)
+			Rigidbody rb = gameObject.GetComponent<Rigidbody>();
+			rb.useGravity = false;
+			rb.velocity = Vector3.zero;//This sets x, y, and z velocity to zero
+			rb.angularVelocity = Vector3.zero;
+
+			//Reposition the shape to the startPosition we stored in the Start function
+			gameObject.transform.position = startPosition;
+			//Reorient the shape to the startRotation we stored in the Start function
+			gameObject.transform.rotation = startRotation;
+		}
+	}
+
+	//This function is called once per frame and it draws User Interface elements.
+	//Don't worry too much about this right now, but it is useful to get our force
+	//printing to the screen.
+	private void OnGUI()
+	{
+		GUI.Label(new Rect(50, 50, 200, 200), launchForce.ToString());
+
+		if (playerWon) {
+			GUI.Label(new Rect(250, 250, 200, 200), "You win!");
+		}
+	}
+}
