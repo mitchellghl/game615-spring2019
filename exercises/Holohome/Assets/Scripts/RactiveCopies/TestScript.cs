956f1f0858b1cbb76e615fd07836c77ea2ddc988
mitchellghl <46975869+mitchellghl@users.noreply.github.com>
2019-03-28 12:57:52 -0400

    working tts

---
+++ exercises/Holohome/Assets/Scripts/RactiveCopies/TestScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+using System.Linq;
+
+
+public class TestScript : MonoBehaviour
+{
+    KeywordRecognizer keywordRecognizer;
+    Dictionary<string, System.Action> keywords = new Dictionary<string, System.Action>();
+
+    // Use this for initialization
+    private void KeywordRecognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        System.Action keywordAction;
+        // if the keyword recognized is in our dictionary, call that Action.
+        if (keywords.TryGetValue(args.text, out keywordAction))
+        {
+            keywordAction.Invoke();
+        }
+    }
+    void Start()
+    {
+
+        //Create keywords for keyword recognizer
+
+        keywords.Add("one", () =>
+        {
+            Debug.Log("You said one.");
+        });
+
+        keywords.Add("two", () =>
+        {
+            Debug.Log("You said two.");
+        });
+
+        keywordRecognizer = new KeywordRecognizer(keywords.Keys.ToArray());
+        keywordRecognizer.OnPhraseRecognized += KeywordRecognizer_OnPhraseRecognized;
+        keywordRecognizer.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ exercises/dumb/Assets/ThingScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThingScript : MonoBehaviour
+{
+    //A class is a template description of an object with 
+    //properties (variables) and methods (functions).
+    //Classes are instantiated into objects.
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
